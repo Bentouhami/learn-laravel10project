7cabbcc37d903a535c1b0830590df3f998c9fceb
Bentouhami Faisal <faisal.bentouhami@gmail.com>
2023-11-30 15:11:47 +0100

    Routes slug - where - regex - Request

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,19 @@ Route::get('/', function () {
 Route::get('/blog', function(Request $resquest){
 
     return [
-        'name' => $resquest->input('name', 'john Doe'),
-        'content' => 'This is a blog post about Laravel.'
+        "name" => $resquest->input('name', 'john Doe'),
+        "content" => "This is a blog post about Laravel."
     ];
 });
+
+Route::get('/blog/{slug}-{id}', function(string $slug, string $id, Request $request){
+    return [
+        'slug' => $slug,
+        'id' => $id,
+
+        'name' => $request->input('name')
+    ];
+})->where([
+    'id' => '[0-9]+', // Only numbers
+    'slug' => '[a-z0-9\-]+'  // Any letters and number (lowercase or uppercase)
+]);

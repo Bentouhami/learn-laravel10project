a5a38885596a7a99e2eb7b9187a222e9b2cfec2f
Bentouhami Faisal <faisal.bentouhami@gmail.com>
2023-11-30 14:48:41 +0100

    Routers with params

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/blog', function(Request $resquest){
+
+    return [
+        'name' => $resquest->input('name', 'john Doe'),
+        'content' => 'This is a blog post about Laravel.'
+    ];
 });
